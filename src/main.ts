0da40913a32be24077b3286939b948ec10f1cf7f
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-28 15:53:13 +0530

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   .setVersion('0.1')
   .build();
 
+  //setup swagger
   const document = SwaggerModule.createDocument(app,config);
   SwaggerModule.setup('api', app, document);
 
